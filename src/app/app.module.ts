4f9824d30ab89e50e7db3c43fb6b29365c07232c
AhamedBilal <bilalazmy0@gmail.com>
2020-09-04 21:51:13 +0530

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { VerificationComponent } from './components/verification/verification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ResetPasswordComponent,
+    VerificationComponent
   ],
   imports: [
     BrowserModule,
